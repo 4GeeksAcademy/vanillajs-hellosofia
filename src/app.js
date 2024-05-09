0d3f841838eac6e2a25d21a3cc25e550425f8f25
Sofia28062001 <163517593+Sofia28062001@users.noreply.github.com>
2024-05-09 16:51:36 +0000

    first Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+document.getElementById("texto").innerHTML;
+ 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let whoindex = Math.floor(Math.random() * who.lenght);
+let actionindex = Math.floor(Math.random() * action.lenght);
+let whatindex = Math.floor(Math.random() * what.lenght);
+let whenindex = Math.floor(Math.random() * when.lenght);
+
+return (
+  who[whoindex] +`npm install`.
+  ""+
+  action[actionindex] +
+  ""+
+  what[whatindex] +
+  ""+
+  when[whenindex] +
+  ""+
+)
